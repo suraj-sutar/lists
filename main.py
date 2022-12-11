ce5850065be28817ec5b947bbd5dd0997556fead
Suraj sutar <66626903+suraj-sutar@users.noreply.github.com>
2022-12-11 05:51:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# friends = ["suraj", "kshinath", "prayag"]
+# print(friends[0])
+# print(friends[1])
+
+# friends = [["suraj", 61], ["prajwal", 19], ["jay", 54], ["tanishq", 1],
+#            ["sandip", 25], ["kashinath", 7]]
+
+# print(friends[0][0])
+# print(friends[1][0])
+# print(friends[2])
+
+# print(friends)
+
+# friends = ["suraj", "kashinath", "prayag"]
+# # how to add a data
+# # you can add the data using append keyword
+
+# friends.append("chetan")
+# print(friends)
+
+# then you have to remove some data from the list that is present in your list 
+# you can use remove function or keyword
+
+friends = ["suraj", "kashinath", "prayag"]
+friends.remove("kashinath")
+print(friends)
